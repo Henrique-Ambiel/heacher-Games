270c40668dcbc53db3ec62329374c6e4eb7996ff
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-09-05 11:49:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Função que executa a pesquisa do usuário
+function pesquisar(){
+   
+//Acessa as coisas que estão no documento html e pega o ID resultados-pesquisa
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+//Inicializa uma string vazia para armazenar resultados
+let resultados = ""
+
+//Itera sobre cada dado da lista
+for (let dado of dados) {
+    //Lê e cria um novo elemento HTML para cada resultado
+    resultados += `
+    <div class = "item-resultado"> 
+        <h2>
+       <a href="#" target="_blank">${dados.titulo}</a>
+        </h2>
+        <p class = "descricao-meta">${dados.descricao}</p>
+        <a href=${dados.link} target="_blank">Trailer</a>
+    </div>
+    `
+}
+
+//Atribui os resultados
+section.innerHTML = resultados
+
+}
---
+++ dados.js
@@ -0,0 +1,14 @@
+//Lista de dados dos projetos da Heacher Games
+let dados = [
+{
+    titulo : "Espadelha",
+    descricao : `Um Battle Royale de 2 a 6 pessoas que se passa dentro de um apiário e cada jogador, 
+    em posse de 4 miniaturas de abelhas, precisará chegar até o meio do mapa e lutar 
+    para sobreviver dos outros jogadores e do fumo.`,
+    link : "https://www.youtube.com/watch?v=FVoFMXPLx9g"    
+},
+{
+    titulo : "Projeto secreto",
+    descricao : "Projeto em estágio de idealização e desenvolvimento.",
+}
+];
