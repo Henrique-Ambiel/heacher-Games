c08db1bc4f86dd1ca2d9ef05542097b49e0aad17
Henrique Ambiel <henriqueambiel@gmail.com>
2024-09-06 09:34:46 -0300

    Finalizado

--- app.js
+++ app.js
@@ -2,21 +2,45 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    //Não exibe informações se não tiver nada na barra de pesquisa
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Você não digitou nada</p> "
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+       if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
         // Cria um novo elemento HTML para cada resultado
         resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Trailer</a>
-            </div>
-        `;
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Trailer</a>
+        </div>
+    `;
+       }
+       
+    }
+
+    if (!resultados){
+        section.innerHTML = "<p>Esse jogo não está aqui</p> "
+        return
     }
 
     // Atribui os resultados gerados à seção HTML
--- dados.js
+++ dados.js
@@ -5,11 +5,13 @@ let dados = [
     descricao : `Um Battle Royale de 2 a 6 pessoas que se passa dentro de um apiário e cada jogador, 
     em posse de 4 miniaturas de abelhas, precisará chegar até o meio do mapa e lutar 
     para sobreviver dos outros jogadores e do fumo.`,
-    link : "https://www.youtube.com/watch?v=FVoFMXPLx9g"    
+    link : "https://www.youtube.com/watch?v=FVoFMXPLx9g",
+    tags :   "jogo tabuleiro hibrido diversao família familia"  
 },
 {
     titulo : "Projeto secreto",
     descricao : "Projeto em estágio de idealização e desenvolvimento.",
     link: "",
+    tags : "escondido jogo novidades"
 }
 ];
